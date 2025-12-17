75dbc2f86cbe220a0590d36eabca30888ab99bc6
Devarshi Desale <devarshibdesale15@gmail.com>
2025-12-17 17:09:08 +0000

    display of dynamic data on web page

--- app.py
+++ app.py
@@ -1,31 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 ACCOMODATIONS = [
    {
-      "type": "hostel",
-      "name": "khedekar hostel",
+      "name": "khandu hostel",
       "distance": "800m",
       "rent": "4200/month",
    },
    {
-      "type": "pg",
-      "name": "atmosphere",
+      "name": "stanza living",
       "distance": "1500m",
       "rent": "25000/month",
-   },
-   {
-      "type": "flat",
-      "name": "siddhi residency",
-      "distance": "300m",
-      "rent": "24000/month",
-   }
+   },  
 ]
 
 @app.route('/')
 def hello_world():
-   return render_template('index.html')
+   return render_template('index.html', accomodations= ACCOMODATIONS)
 
+@app.route('/accommodations')
+def list():
+   return jsonify(ACCOMODATIONS)
+   
 if __name__ == '__main__':
    app.run(host='0.0.0.0', debug=True) 
